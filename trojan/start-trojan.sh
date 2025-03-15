1d4fbcbde95599f6fd29474556af132d054f3b1d
nickzhangyi <nickzhangyi@gmail.com>
2025-03-15 10:07:27 -0400

    fix trojan issue

--- trojan/start-trojan.sh
+++ trojan/start-trojan.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Replace environment variables in the template file
-envsubst < /config/config.json.template > /config/config.json
+# Manually replace environment variables in the template file
+sed -e "s|\${TROJAN_REMOTE_ADDR}|$TROJAN_REMOTE_ADDR|g" \
+    -e "s|\${TROJAN_REMOTE_PORT}|$TROJAN_REMOTE_PORT|g" \
+    -e "s|\${TROJAN_PASSWORD}|$TROJAN_PASSWORD|g" \
+    /config/config.json.template > /config/config.json
 
 # Start trojan with the generated config
 exec /usr/local/bin/trojan /config/config.json 
